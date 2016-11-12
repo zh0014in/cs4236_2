e4e4af0a582f673bbced015bc1d9c2abf1128ca3
Zhu Wu <zhu.wu@outlook.com>
2016-11-12 16:52:35 +0800

    use file name to generate compiled js

--- deploy.js
+++ deploy.js
@@ -6,6 +6,9 @@ if (argv.length == 0) {
   process.exit(1);
 }
 
+var solFile = argv[0];
+var rootName = solFile.substring(0, solFile.lastIndexOf('.'));
+
 var fs = require('fs');
 var templatefile = "./template.js";
 var template = fs.readFileSync(templatefile, "utf8");
@@ -13,17 +16,17 @@ var templateAbiFile = "./template-abi.js";
 var templateAbi = fs.readFileSync(templateAbiFile, "utf8");
 
 var exec = require('child_process').exec;
-var cmd = 'solc --bin --abi -o ./bin lottery.sol';
+var cmd = 'solc --bin --abi -o ./bin ' + solFile;
 exec(cmd, function(error, stdout, stderr) {
   // command output is in stdout
   if (!error) {
-    var abi = fs.readFileSync("./bin/lottery.abi", "utf8").trim();
-    var bin = fs.readFileSync("./bin/lottery.bin", "utf8").trim();
+    var abi = fs.readFileSync("./bin/" + rootName + ".abi", "utf8").trim();
+    var bin = fs.readFileSync("./bin/" + rootName + ".bin", "utf8").trim();
     template = template.replace(/\$ABI/, abi);
     template = template.replace(/\$BIN/, bin);
     templateAbi = templateAbi.replace(/\$ABI/, abi);
-    fs.writeFileSync("lottery.js", template, "utf8");
-    fs.writeFileSync("lottery-abi.js", templateAbi, "utf8");
+    fs.writeFileSync(rootName + ".js", template, "utf8");
+    fs.writeFileSync(rootName + "-abi.js", templateAbi, "utf8");
   } else {
     console.error("Solc compilation error!");
     process.exit(1)
